5e9f0a7f2f39f89fca96a7496280905088446c06
Matt French <matthewfrench019@gmail.com>
2020-05-22 10:10:22 +0100

    Basic Form Controller

---
+++ Helpdesk.Website/Controllers/FormController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Helpdesk.Website.Models;
+using Helpdesk.Website.Services;
+
+namespace Helpdesk.Website.Controllers
+{
+    [ApiController]
+    [Route("api/form")]
+    public class FormController : Controller
+    {
+        [HttpPost]
+        public async Task<bool> GetForm([FromHeader] string firebaseJWT, [FromBody] Form form)
+        {
+            Console.WriteLine(firebaseJWT, form);
+            return await FirebaseAuthService.CheckUserToken(firebaseJWT);
+        }
+    }
+}
