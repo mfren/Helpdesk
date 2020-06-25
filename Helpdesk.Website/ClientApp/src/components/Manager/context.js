336f7c42507f132c9fa2e195ed8299e4e6a608b7
Matt French <matthewfrench019@gmail.com>
2020-06-25 14:26:50 +0100

    Manager Changes - Current users are now subscribed to by components - withManager provides the users in the 'users' prop

--- Helpdesk.Website/ClientApp/src/components/Manager/context.js
+++ Helpdesk.Website/ClientApp/src/components/Manager/context.js
@@ -2,10 +2,4 @@ import React from 'react';
 
 const ManagerContext = React.createContext(null);
 
-export const withManager = Component => props => (
-    <ManagerContext.Consumer>
-        {manager => <Component {...props} manager={manager} />}
-    </ManagerContext.Consumer>
-);
-
 export default ManagerContext;
---
+++ Helpdesk.Website/ClientApp/src/components/Manager/withManager.js
@@ -0,0 +1,52 @@
+﻿import React, {Component} from "react";
+import ManagerContext from "./context";
+
+const withManagerBase = Component => props => (
+    <ManagerContext.Consumer>
+        {manager => <Component {...props} manager={manager} />}
+    </ManagerContext.Consumer>
+);
+
+const withManager = Component => {
+    class WithManager extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                currentUser: null,
+                currentAdmin: null,
+            }
+        }
+        
+        componentDidMount() {
+            this.userListener = this.props.manager.userAuth.onAuthStateChanged(
+                authUser => {
+                    authUser
+                        ? this.setState({currentUser: authUser})
+                        : this.setState({currentUser: null});
+                }
+            );
+            
+            this.adminListener = this.props.manager.adminAuth.onAuthStateChanged(
+                authAdmin => {
+                    authAdmin
+                        ? this.setState({currentAdmin: authAdmin})
+                        : this.setState({currentAdmin: null});
+                }
+            )
+        }
+        
+        componentWillUnmount() {
+            this.userListener();
+            this.adminListener();
+        }
+        
+        render() {
+            return (
+                <Component {...this.props} users={this.state} />
+            )
+        }
+    }
+    return withManagerBase(WithManager);
+};
+
+export default withManager;
