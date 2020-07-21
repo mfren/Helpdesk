1dc67be67b30617bda6baf7573378cda1f015758
Matt French <matthewfrench019@gmail.com>
2020-07-21 14:25:32 +0100

    Basic Homepage

--- Helpdesk.Website/ClientApp/src/components/Home/Home.js
+++ Helpdesk.Website/ClientApp/src/components/Home/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import PageLimit from "../Layouts/PageLimit";
 import {withAuth} from "../Manager/withAuth";
-import * as CONDITIONS from '../../constants/authConditions.js';
+import * as CONDITIONS from '../../constants/authConditions';
+
+function UserHome(props) {
+    return (
+        <PageLimit>
+            <h1>Hello User!</h1>
+        </PageLimit>
+    )
+}
+
+function AdminHome(props) {
+    return (
+        <PageLimit>
+            <h1>Hello Admin!</h1>
+        </PageLimit>
+    )
+}
+
 
 function HomeBase(props) {
     return (
-        <PageLimit>
-            <h1>Hello World!</h1>
-        </PageLimit>
+        <div>
+            {props.isAdmin === true ? <AdminHome/> : <UserHome/> }
+        </div>
     )
 }
 
