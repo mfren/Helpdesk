c37618f0947da54ae6ed37ae042ff9383d9d8bd4
Matt French <matthewfrench019@gmail.com>
2020-07-21 16:03:51 +0100

    Added Top Margin

--- Helpdesk.Website/ClientApp/src/components/Layouts/PageLimit/PageLimit.js
+++ Helpdesk.Website/ClientApp/src/components/Layouts/PageLimit/PageLimit.js
@@ -1,9 +1,16 @@
 ﻿import React from "react";
-import { Container } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    pageLimit: {
+        marginTop: theme.spacing(3),
+    },
+}));
 
 export function PageLimit(props) {
+    const classes = useStyles()
     return (
-        <Container maxWidth="lg">
+        <Container className={classes.pageLimit} maxWidth="lg">
             {props.children}
         </Container>
     )
