19c38d32a7814cfd0bb130cec33cf0d913e5d12f
Matt French <matthewfrench019@gmail.com>
2020-07-30 08:37:24 +0100

    Flexbox Layout

--- Helpdesk.Website/ClientApp/src/App.js
+++ Helpdesk.Website/ClientApp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Route, Switch} from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
 import SignIn from "./components/SignIn";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
@@ -7,25 +8,59 @@ import SignUp from "./components/SignUp";
 import Manager, {ManagerContext} from "./components/Manager";
 import * as ROUTES from "./constants/routes";
 
-
-export default class App extends Component {
-
-    render () {
-        return (
-            <ManagerContext.Provider value={new Manager()}>
-                <Navigation/>
-                <Switch>
-                    <Route path={ROUTES.SIGN_IN}>
-                        <SignIn/>
-                    </Route>
-                    <Route path={ROUTES.SIGN_UP}>
-                        <SignUp/>
-                    </Route>
-                    <Route path={ROUTES.HOME}>
-                        <Home/>
-                    </Route>
-                </Switch>
-            </ManagerContext.Provider>
-        );
+const useStyles = makeStyles((theme) => ({
+    mainContainer: {
+        display: "flex",
+        flexFlow: "column",
+        height: "100%",
+    },
+    headerContainer: {
+        flexGrow: 0,
+        flexShrink: 1,
+        flexBasis: "auto",
+    },
+    contentContainer: {
+        flexGrow: 1,
+        flexShrink: 1,
+        flexBasis: "auto",
+    },
+    footerContainer: {
+        flexGrow: 0,
+        flexShrink: 1,
+        flexBasis: "40px",
+    },
+    switch: {
+        height: "100%"
     }
+}));
+
+export default function App() {
+    const classes = useStyles();
+    
+    return (
+        <ManagerContext.Provider value={new Manager()}>
+            <div className={classes.mainContainer}>
+                <div className={classes.headerContainer}>
+                    <Navigation/>
+                </div>
+                <div className={classes.contentContainer}>
+                    <Switch className={classes.switch}>
+                        <Route path={ROUTES.SIGN_IN}>
+                            <SignIn/>
+                        </Route>
+                        <Route path={ROUTES.SIGN_UP}>
+                            <SignUp/>
+                        </Route>
+                        <Route path={ROUTES.HOME}>
+                            <Home/>
+                        </Route>
+                    </Switch>
+                </div>
+                <div className={classes.footerContainer}>
+                    
+                </div>
+            </div>
+            
+        </ManagerContext.Provider>
+    );
 }
