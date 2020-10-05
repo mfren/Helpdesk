8a65c3cf6e6dece444cacc4e90c4f5f5f18eb722
Matt French <matthewfrench019@gmail.com>
2020-10-05 14:05:35 +0100

    404 Page

---
+++ Helpdesk.Website/ClientApp/src/components/NoMatch/NoMatch.js
@@ -0,0 +1,17 @@
+﻿import React from "react";
+import {
+    useLocation
+} from "react-router-dom";
+
+
+export function NoMatch(props) {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h3>
+                No match for <code>{location.pathname}</code>
+            </h3>
+        </div>
+    );
+}
