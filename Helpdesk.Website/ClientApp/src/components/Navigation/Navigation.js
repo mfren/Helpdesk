51d5d6689fa20d300079010be2ef83a762e0b2d9
Matt French <matthewfrench019@gmail.com>
2020-12-23 11:45:58 +0000

    Added proper title

--- Helpdesk.Website/ClientApp/src/components/Navigation/Navigation.js
+++ Helpdesk.Website/ClientApp/src/components/Navigation/Navigation.js
@@ -45,11 +45,8 @@ function NavigationBase(props) {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        Photos
+                        Matt's Helpdesk
                     </Typography>
                     {auth && (
                         <div>
